7eee2a463c8ffb7ea28a1f9d8d96e0eebb82d5cf
Adam CAVILLON <dandadounet@gmail.com>
2021-08-09 22:27:13 +0200

    Added PlayerConnection public channel

--- src/main/java/net/gameinbox/voidserver/server/PlayerConnection.java
+++ src/main/java/net/gameinbox/voidserver/server/PlayerConnection.java
@@ -2,15 +2,18 @@ package net.gameinbox.voidserver.server;
 
 import io.netty.channel.Channel;
 import io.netty.util.AttributeKey;
+import net.gameinbox.voidserver.player.VoidPlayer;
 import net.gameinbox.voidserver.server.packet.CommunicationState;
 import net.gameinbox.voidserver.server.packet.EncodedPacket;
 import net.gameinbox.voidserver.server.packet.Packet;
 
 public class PlayerConnection {
 
-    private final Channel channel;
+    public final Channel channel;
 
     public CommunicationState communicationState;
+
+    private VoidPlayer parent;
 
     public PlayerConnection(Channel channel, CommunicationState communicationState) {
         this.channel = channel;
@@ -21,6 +24,14 @@ public class PlayerConnection {
         if(channel.isWritable()) {
             channel.writeAndFlush(packet.getEncodedPacket());
         }
+    }
+
+    public VoidPlayer getParent() {
+        return parent;
+    }
+
+    public void setParent(VoidPlayer parent) {
+        this.parent = parent;
     }
 
 }
