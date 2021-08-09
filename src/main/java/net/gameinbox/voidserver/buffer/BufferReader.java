1f472d430c4490eea9381006b3d042c9020aee28
Adam CAVILLON <dandadounet@gmail.com>
2021-08-09 12:44:40 +0200

    Created BufferReader and BufferWriter utility classes

---
+++ src/main/java/net/gameinbox/voidserver/buffer/BufferReader.java
@@ -0,0 +1,62 @@
+package net.gameinbox.voidserver.buffer;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.nio.charset.StandardCharsets;
+
+public class BufferReader {
+
+    private final ByteBuf buffer;
+
+    private BufferReader(ByteBuf buffer) {
+        this.buffer = buffer;
+    }
+
+    public static BufferReader newReader(ByteBuf buffer) {
+        return new BufferReader(buffer);
+    }
+
+    public int readVarInt() {
+        int i = 0;
+        int j = 0;
+
+        byte b0;
+
+        do {
+            b0 = buffer.readByte();
+            i |= (b0 & 127) << j++ * 7;
+            if (j > 5) {
+                throw new RuntimeException("VarInt too big");
+            }
+        } while ((b0 & 128) == 128);
+
+        return i;
+    }
+
+    public long readVarLong() {
+        long value = 0;
+        int bitOffset = 0;
+        byte currentByte;
+        do {
+            if (bitOffset == 70) throw new RuntimeException("VarLong is too big");
+
+            currentByte = buffer.readByte();
+            value |= (currentByte & 0b01111111) << bitOffset;
+
+            bitOffset += 7;
+        } while ((currentByte & 0b10000000) != 0);
+
+        return value;
+    }
+
+    public String readString() {
+        int length = readVarInt();
+        return buffer.readCharSequence(length, StandardCharsets.UTF_8).toString();
+    }
+
+    public ByteBuf getBuffer() {
+        return buffer;
+    }
+
+}
---
+++ src/main/java/net/gameinbox/voidserver/buffer/BufferWriter.java
@@ -0,0 +1,59 @@
+package net.gameinbox.voidserver.buffer;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.nio.charset.StandardCharsets;
+
+public class BufferWriter {
+
+    private final ByteBuf buffer;
+
+    private BufferWriter(ByteBuf buffer) {
+        this.buffer = buffer;
+    }
+
+    public static BufferWriter newWriter() {
+        return new BufferWriter(Unpooled.buffer());
+    }
+
+    public static BufferWriter newWriter(ByteBuf buffer) {
+        return new BufferWriter(buffer);
+    }
+
+    public BufferWriter writeVarInt(int value) {
+        do {
+            byte currentByte = (byte) (value & 0b01111111);
+
+            value >>>= 7;
+            if (value != 0) currentByte |= 0b10000000;
+
+            buffer.writeByte(currentByte);
+        } while (value != 0);
+        return this;
+    }
+
+    public BufferWriter writeVarLong(long value) {
+        do {
+            byte currentByte = (byte) (value & 0b01111111);
+
+            value >>>= 7;
+            if (value != 0) currentByte |= 0b10000000;
+
+            buffer.writeByte(currentByte);
+        } while (value != 0);
+        return this;
+    }
+
+    public BufferWriter writeString(boolean hasLengthPrefix, String str) {
+        if(hasLengthPrefix)
+            writeVarInt(str.length());
+        buffer.writeCharSequence(str, StandardCharsets.UTF_8);
+        return this;
+    }
+
+    public ByteBuf getBuffer() {
+        return buffer;
+    }
+
+}
