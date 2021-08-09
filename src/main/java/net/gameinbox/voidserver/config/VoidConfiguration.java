3a018792f0ae21a5716f2c53a19ffe3416590b5f
Adam CAVILLON <dandadounet@gmail.com>
2021-08-09 20:30:15 +0200

    Deleted configuration usePlugins

--- src/main/java/net/gameinbox/voidserver/config/VoidConfiguration.java
+++ src/main/java/net/gameinbox/voidserver/config/VoidConfiguration.java
@@ -8,21 +8,12 @@ public class VoidConfiguration {
     int serverPort = 25565;
 
     /**
-     * Is limbo using plugins or being vanilla
-     */
-    boolean usePlugins = false;
-
-    /**
      * Max number of players currently connected to limbo server (-1 for infinite)
      */
     int maxPlayers = -1;
 
     public int getServerPort() {
         return serverPort;
-    }
-
-    public boolean usePlugins() {
-        return usePlugins;
     }
 
     public int maxPlayers() {
